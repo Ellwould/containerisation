5d2c46d769042da6c49de579e974fd2b2af694c3
Elliot Michael Keavney <103308993+Ellwould@users.noreply.github.com>
2025-02-01 16:27:05 +0000

    Create pivot-root-container.sh

---
+++ Ubuntu/pivot-root-container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+   #######      ###       ###
+  #########      ###     ###
+  ###             ###   ###       [Container View] Pivot root shell script
+  ###              ### ###        Run script after create-container.sh script
+  #########   ###   #####   ###
+   #######    ###    ###    ###
+
+cd /root/container-view-research/bash/rootfs;
+
+mkdir /root/container-view-research/bash/rootfs/old_root;
+
+mount --bind /root/container-view-research/bash/rootfs /root/container-view-research/bash/rootfs;
+
+cd /root/container-view-research/bash/rootfs;
+
+pivot_root . ./old_root;
+
+cd /;
+
+mount -t proc proc /proc;
+
+umount -l old_root;
+
+rm -r /old_root;
