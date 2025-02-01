6c1f9e0d9e573b699708f814f4fa7068439692a1
Elliot Michael Keavney <103308993+Ellwould@users.noreply.github.com>
2025-02-01 16:22:54 +0000

    Create create-container.sh - Not finished, work in progress.
    Not finished, work in progress.

---
+++ Ubuntu/create-container.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+   #######      ###       ###
+  #########      ###     ###
+  ###             ###   ###       [Container View] Create Container shell script
+  ###              ### ###
+  #########   ###   #####   ###
+   #######    ###    ###    ###
+
+
+
+    ##################
+   ##                ##
+  ###   cgroups v2   ###
+   ##                ##
+    ##################
+
+# Create control group, they do not persist after reboot by default
+cgcreate -g cpu,memory:/container-view;
+
+# Cannot edit write to files in /sys/fs/cgroup with text editer
+# Using the tool
+cgset -r cpu.max="10000 100000" container-view;
+cgset -r memory.max="500M" container-view;
+cgset -r memory.swap.max="0" container-view;
+
+cd /root/container-view-research/bash;
+
+   ##################
+  ##                ##
+ ###   namespaces   ###
+  ##                ##
+   ##################
+
+unshare --uts --pid --mount --mount-proc --net --ipc --cgroup --fork /bin/bash;
