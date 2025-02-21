381dfcd567abe7396c2899897828e572744feb04
Elliot Michael Keavney <103308993+Ellwould@users.noreply.github.com>
2025-02-21 21:43:17 +0000

    Update cgroup-namespace.sh

--- Ubuntu/cgroup-namespace.sh
+++ Ubuntu/cgroup-namespace.sh
@@ -17,19 +17,20 @@
 
 # Create control group, they do not persist after reboot by default
 cgcreate -g cpu,memory:/container-view;
+cgcreate -g cpu,memory:/container-view/container1;
 
 # Cannot edit write to files in /sys/fs/cgroup with text editer
-# Using the tool
-cgset -r cpu.max="10000 100000" container-view;
-cgset -r memory.max="500M" container-view;
-cgset -r memory.swap.max="0" container-view;
+# Using the tool 
+cgset -r cgroup.subtree_control="+memory +cpu" container-view;
+cgset -r cpu.max="10000 100000" container-view/container1;
+cgset -r memory.max="500M" container-view/container1;
+cgset -r memory.swap.max="0" container-view/container1;
+cgset -r cgroup.procs=$pid container-view/container1;
 
-cd /root/container-view-research/bash;
+    ##################
+   ##                ##
+  ###   namespaces   ###
+   ##                ##
+    ##################
 
-   ##################
-  ##                ##
- ###   namespaces   ###
-  ##                ##
-   ##################
-
-unshare --uts --pid --mount --mount-proc --net --ipc --cgroup --fork /bin/bash;
+unshare --time --uts --pid --mount --mount-proc --ipc --net --cgroup --fork /bin/bash;
